a13423dab8ade6d8eb4f5b31b71966e1eb4cbb33
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2025-06-18 21:57:55 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors');
+
 
 const app = express();
 const cors = require('cors');
